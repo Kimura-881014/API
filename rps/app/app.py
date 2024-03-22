4c20de7cbce017a57560ab22bb5e9200e331de71
Kimura-881014 <kimura.881014@gmail.com>
2024-03-22 12:24:39 +0900

    del .env

--- rps/app/app.py
+++ rps/app/app.py
@@ -1,15 +1,27 @@
 from flask import Flask
+from flask import jsonify
 import random
+import time
+import hashlib
+import os
+from dotenv import load_dotenv
 
 
 app = Flask(__name__)
 
+
+load_dotenv()
+key = os.environ['MY_SECRET_KEY']
+
 @app.route("/")
 def hello_world():
     num = random.randrange(3)
-    options = {0: "グー", 1: "チョキ", 2: "パー"}
+    options = {0: "Rock", 1: "Paper", 2: "Scissors"}
     payload = options.get(num)
-    return payload
+    _time = str(int(time.time()))
+    hash_obj = hashlib.sha256((_time + payload + key).encode())
+    signature = hash_obj.hexdigest()
+    return jsonify({"time": _time, "payload":payload, "signature":signature})
 
 
 if __name__ == ('__main__'):
